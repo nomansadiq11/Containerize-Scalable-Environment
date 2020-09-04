3044179a66dcb8940c294f90ccdb8df52c5bc367
nomansadiq11 <nomi.cs786@gmail.com>
2020-09-04 14:54:41 +0400

    proxy detection added

--- ProxyScaning/act4ste1.py
+++ ProxyScaning/act4ste1.py
@@ -1,26 +1,29 @@
-# Import libraries
-import requests
-# import urllib.request
-# import time
-# from bs4 import BeautifulSoup
+import urllib.request , socket
 
-# Set the URL you want to webscrape from
-url = 'http://csec.rit.edu'
+socket.setdefaulttimeout(180)
 
-# Connect to the URL
-response = requests.get(url)
+# read the list of proxy IPs in proxyList
+proxyList = ['140.82.61.218:8080'] # there are two sample proxy ip
 
-print(response)
-# Parse HTML and save to BeautifulSoup object¶
-# soup = BeautifulSoup(response.text, "html.parser")
+def is_bad_proxy(pip):    
+    try:        
+        proxy_handler = urllib.request.ProxyHandler({'http': pip})        
+        opener = urllib.request.build_opener(proxy_handler)
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        urllib.request.install_opener(opener)        
+        sock=urllib.request.urlopen('http://www.google.com')  # change the url address here
+        #sock=urllib.urlopen(req)
+    except urllib.error.HTTPError as e:        
+        print('Error code: ', e.code)
+        return e.code
+    except Exception as detail:
 
-# # To download the whole data set, let's do a for loop through all a tags
-# line_count = 1 #variable to track what line you are on
-# for one_a_tag in soup.findAll('a'):  #'a' tags are for links
-#     if line_count >= 36: #code for text files starts at line 36
-#         link = one_a_tag['href']
-#         download_url = 'http://web.mta.info/developers/'+ link
-#         urllib.request.urlretrieve(download_url,'./'+link[link.find('/turnstile_')+1:]) 
-#         time.sleep(1) #pause the code for a sec
-#     #add 1 for next line
-#     line_count +=1
+        print( "ERROR:", detail)
+        return 1
+    return 0
+
+for item in proxyList:
+    if is_bad_proxy(item):
+        print ("Proxy is not working ", item)
+    else:
+        print (item, "proxy discovered")
